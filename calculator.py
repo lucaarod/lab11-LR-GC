db2dc04fe4cba4a75be44a65508a8134ac815992
lucaarod <lucadrodriguez07@gmail.com>
2025-11-12 11:20:38 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,34 @@
+# https://github.com/lucaarod/lab11-LR-GC
+# Partner 1: Luca Rodriguez
+# Partner 2: Guilherme Carvalheira
+
 """
 calculator.py
 - Defines functions used to create a simple calculator
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
+import math
+# Part 1 
+def add(a, b):
+    return a + b
 
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b / a
+
+def log(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
 
